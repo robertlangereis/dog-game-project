dbffdf8fb91a063cca8314ffcb77ce0e85d66d68
maugares <mauro.garciaesteban@gmail.com>
2019-04-08 20:36:59 +0200

    in listReducer.js add default reducer function

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -3,3 +3,11 @@ import GET_LIST from '../actions/types'
 const initialState = {
   dogBreeds: null
 }
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    
+    default:
+      return state
+  }
+}
