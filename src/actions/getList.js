2738e2ca572e70bd75bcffc84c75c23986f47f72
maugares <mauro.garciaesteban@gmail.com>
2019-04-08 20:31:46 +0200

    create action getList to get-dispatch type:GET_LIST with payload: breeds

---
+++ src/actions/getList.js
@@ -0,0 +1,15 @@
+import GET_LIST from './types'
+import { request } from 'superagent'
+
+export const getList = () => dispatch => {
+  request
+    .get('https://dog.ceo/api/breeds/list/all')
+    .then(response => {
+      const breeds = Object.keys(response.body.message)
+      dispatch({
+        type: GET_LIST,
+        payload: breeds
+      })
+    })
+    .catch(console.error)
+}
