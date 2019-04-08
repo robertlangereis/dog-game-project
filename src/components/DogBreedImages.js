d3c24cbf18c73e844781be7d5e8b8fb52b6e2937
maugares <mauro.garciaesteban@gmail.com>
2019-04-08 16:12:06 +0200

    pull back a wrong commit

--- src/components/DogBreedImages.js
+++ src/components/DogBreedImages.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function DogBreedImages(props) {
+export default function DogBreedImages (props) {
     console.log('props test:', props)
     const { images } = props
     return (
         <div className="dog-breed-images">
             <h1>Dogs Breed Images</h1>
 
-            This page will show images of the {props.breed} breed.
+            This page will show images of the { props.breed } breed.
 
             <Link to="/">Go back to the index</Link>
 
             <div>
-                {images &&
-                    console.log('Logging the images: ', images.map(url => {
-                        return {for (let i=0; i < 10; i++) {
-                            return <img src={url} alt="Dog" />
-                        }}}))}
-                {!images && 'Loading...'}
+                { images && images.map(url => <img src={ url } alt="Dog" />) }
+                { !images && 'Loading...' }
             </div>
         </div>
     )
