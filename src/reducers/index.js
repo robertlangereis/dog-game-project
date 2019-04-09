9b92fb932da3361a2cfb4a0a2c4b72fd8cb62e2a
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-04-09 17:12:20 +0200

    randomimage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
-// import reducer from './reducer'
+import randomImage from './gameReducer'
 import dogBreeds from './listReducer'
 
 export default combineReducers({
-    dogBreeds 
+    dogBreeds,
+    randomImage
 })
