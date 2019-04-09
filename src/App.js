8ba18abc3a87f07fb6dc0b41f46c657925c4c679
maugares <mauro.garciaesteban@gmail.com>
2019-04-09 15:01:30 +0200

    fetching doglist from API working by using action-reducer combination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
-import DogsListContainer2 from './components/allInOne';
 import { Route } from 'react-router-dom'
 import DogBreedImagesContainer from './components/DogBreedImagesContainer'
 import DogGameContainer from './components/DogGame/DogGameContainer'
@@ -12,7 +11,6 @@ class App extends Component {
       <div className="App">
       <main>
       <Route exact path="/" component={DogsListContainer} />
-      {/* <Rzoute exact path="/" component={DogsListContainer2} /> */}
       <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
       <Route path="/dog-game/" component={DogGameContainer} />
       </main>
--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
-// import request from 'superagent'
 import DogsList from './DogsList'
 import { connect } from 'react-redux'
 import { getList } from '../actions/setList'
 
 
 class DogsListContainer extends Component {
-  // state = { dogBreeds: null }
-
   componentDidMount() {
     this.props.getList()
   }
 
   render() {
-    // console.log('in the render:', this.props.getList())
-    // return <h1>Hi</h1>
     return <DogsList dogBreeds={this.props.dogBreeds} />
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state)
-
   return { dogBreeds: state.dogBreeds }
 }
 
-console.log('mapStateToProps:',)
 export default connect(mapStateToProps, { getList })(DogsListContainer)
