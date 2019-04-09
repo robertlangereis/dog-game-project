19e5ba6920a75b44a16b83e2d3d17713756c5ec4
maugares <mauro.garciaesteban@gmail.com>
2019-04-09 16:25:37 +0200

    in App.js add routing to the HomePageContainer.js at path="/"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import DogsListContainer from './components/DogListPage/DogsListContainer';
 import { Route } from 'react-router-dom'
 import DogBreedImagesContainer from './components/DogBreedImagesContainer'
 import DogGameContainer from './components/DogGame/DogGameContainer'
+import DogsListContainer from './components/DogListPage/DogsListContainer'
+import HomePageContainer from './components/HomePage/HomePageContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <main>
+          <Route exact path="/" component={HomePageContainer}/>
           <Route exact path="/dog-list" component={DogsListContainer} />
           <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
-          <Route path="/dog-game/" component={DogGameContainer} />
+          <Route exact path="/dog-game/" component={DogGameContainer} />
         </main>
       </div>
     );
