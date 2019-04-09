e460b8f700293c07cf9f16f70edbd09e7e7972c1
maugares <mauro.garciaesteban@gmail.com>
2019-04-09 16:29:29 +0200

    in HomePageContainer.js add a link to path="/dog-list"

--- src/components/HomePage/HomePageContainer.js
+++ src/components/HomePage/HomePageContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class HomePageContainer extends Component {
   render() {
     return (
       <div>
-        
+        <div>
+          <Link to='/dog-list'>Link to the learning page</Link>
+        </div>
       </div>
     )
   }
