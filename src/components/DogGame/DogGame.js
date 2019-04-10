f74937dd5449b0677594cf1382f0d1c42818f682
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-04-10 12:11:21 +0200

    two random dogs on buttons

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -5,14 +5,24 @@ export default class DogGame extends Component {
 
 
     render() {
-        const { randomImage } = this.props
+        const { randomImage, randomBreed, randomBreed2 } = this.props
 
         return (
-        <div className='dog-game'>
-        <h1>Dog Game</h1>
+            <div className='dog-game'>
+                <h1>Dog Game</h1>
         
-        {!randomImage && 'Loading...'}
-        {randomImage && <img src={this.props.randomImage.message} alt='RandomImage'></img>}
+                {!randomImage && 'Loading...'}
+                {randomImage && <img src={randomImage.message} alt='RandomImage'></img>}
+                {!randomBreed && 'Loading...'}
+                {randomBreed &&
+        <div>
+            <button>{this.props.randomBreed}</button>
+        </div>}
+        {!randomBreed2 && 'Loading...'}
+                {randomBreed2 &&
+        <div>
+            <button>{this.props.randomBreed2}</button>
+        </div>}
         <Link to= "/">Go back to Homepage</Link>
         </div>
     )}
--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -1,6 +1,6 @@
 import { SET_LIST } from '../actions/types'
 
-export default function (state = null, action) {
+export default function (state = [], action) {
   switch (action.type) {
     case SET_LIST:
       const data = action.payload.message
