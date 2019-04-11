a644f688ef5781688c4000ac5abc6f27cbe349f0
maugares <mauro.garciaesteban@gmail.com>
2019-04-11 20:57:03 +0200

    clean-up DogGameContainer

--- src/components/DogGame/DogGameContainer.js
+++ src/components/DogGame/DogGameContainer.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getImages } from '../../actions/imageActions'
 import DogGame from './DogGame'
-import { getWinner } from '../../actions/imageActions'
 
 export default class DogGameContainer extends Component {
   render() {
-    // let test = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
-    // let test2 = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
-    return (
-      <div>
-        {/* <DogGame /> */}
-        <DogGame
-          // class='container'
-          // randomBreed={test}
-          // randomBreed2={test2}
-        />
-      </div>
-    )
+    return <div>
+      <DogGame />
+    </div>
   }
 }
-// const mapStateToProps = state => {
-//   // console.log('DogGameContainer state:', state.dogs)
-//   return {
-//     dogBreeds: state.dogs.dogBreeds,
-//     dogWinnerImage: state.dogs,
-//     dogWinner: state.dogs.dogWinner,
-//     getImages: state.getImages,
-//     dogImage: state.dogImage
-//   }
-// }
-
-// export default connect(mapStateToProps, { getImages, getWinner })(DogGameContainer)
