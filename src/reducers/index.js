d6cd75a066751ae69b32f9edd8b1860a19d1d50e
maugares <mauro.garciaesteban@gmail.com>
2019-04-11 20:48:55 +0200

    adding the performance reducer to the reducer-root

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import dogs from './listReducer'
 import dogImage from './imgReducer'
+import performance from './performance'
 
 export default combineReducers({
     dogs,
     dogImage, 
+    performance
 })
--- src/reducers/performance.js
+++ src/reducers/performance.js
@@ -1,4 +1,4 @@
-import { SET_PERFORMANCE, CALC_PERFORMANCE } from '../actions/setPerformance'
+import { SET_PERFORMANCE, CALC_PERFORMANCE } from '../actions/types'
 
 export default function (state = null, action) {
   switch (action.type) {
