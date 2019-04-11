4ace99629e63405bb85522a1322af5ddf3e8c75d
maugares <mauro.garciaesteban@gmail.com>
2019-04-11 18:44:55 +0200

    winner breed button working

--- src/components/DogGame/DogGameContainer.js
+++ src/components/DogGame/DogGameContainer.js
@@ -4,35 +4,31 @@ import { getImages } from '../../actions/imageActions'
 import DogGame from './DogGame'
 import { getWinner } from '../../actions/imageActions'
 
-class DogGameContainer extends Component {
-
-  componentDidMount() {
-    this.props.getWinner()
-  }
-
+export default class DogGameContainer extends Component {
   render() {
-    let test = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
-    let test2 = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
-    //  const dogWinnerImage = this.props.dogWinnerImage[1]
+    // let test = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
+    // let test2 = this.props.dogBreeds ? this.props.dogBreeds.sort(() => .5 - Math.random()).slice(0, 1) : 'Loading...'
     return (
-      <DogGame
-        dogWinnerImage={this.props.dogWinnerImage}
-        class='container'
-        randomBreed={test}
-        randomBreed2={test2}
-      />
+      <div>
+        {/* <DogGame /> */}
+        <DogGame
+          // class='container'
+          // randomBreed={test}
+          // randomBreed2={test2}
+        />
+      </div>
     )
   }
 }
-const mapStateToProps = state => {
-  // console.log('DogGameContainer state:', state.dogs)
-  return {
-    dogBreeds: state.dogs.dogBreeds,
-    dogWinnerImage: state.dogs,
-    dogWinner: state.dogs.dogWinner,
-    getImages: state.getImages,
-    dogImage: state.dogImage
-  }
-}
+// const mapStateToProps = state => {
+//   // console.log('DogGameContainer state:', state.dogs)
+//   return {
+//     dogBreeds: state.dogs.dogBreeds,
+//     dogWinnerImage: state.dogs,
+//     dogWinner: state.dogs.dogWinner,
+//     getImages: state.getImages,
+//     dogImage: state.dogImage
+//   }
+// }
 
-export default connect(mapStateToProps, { getImages, getWinner })(DogGameContainer)
+// export default connect(mapStateToProps, { getImages, getWinner })(DogGameContainer)
