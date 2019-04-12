0e4585d864b7afe389e8d367e68ba7b99f79b056
maugares <mauro.garciaesteban@gmail.com>
2019-04-12 13:01:53 +0200

    repair performance functionality

--- src/components/DogGame/DogGame.js
+++ src/components/DogGame/DogGame.js
@@ -8,6 +8,7 @@ import { getList } from '../../actions/setList'
 import { getWinner } from '../../actions/imageActions'
 import { connect } from 'react-redux'
 import { setPerformance } from '../../actions/setPerformance';
+import { setPercentage } from '../../actions/setPercentage';
 
 class DogGame extends Component {
 
@@ -31,6 +32,7 @@ class DogGame extends Component {
     nextIfRight = () => {
         rightAnswer();
         this.props.setPerformance();
+        this.props.setPercentage();
         this.props.getList();
         this.props.getWinner();
     }
@@ -39,6 +41,7 @@ class DogGame extends Component {
         wrongAnswer(this.props.dogWinnerImage.dogWinner);
         this.props.getList();
         this.props.getWinner();
+        this.props.setPercentage();
     }
 
     selectOption = (event) => {
@@ -83,6 +86,8 @@ class DogGame extends Component {
         // Randomise the buttons order
         const newArray = [dogWinner, test, test2].sort((a, b) => 0.5 - Math.random())
 
+        this.locState = newArray
+
         return (
             <div className='dog-game'>
                 <header
@@ -119,7 +124,7 @@ class DogGame extends Component {
                         <button onClick={() => { document.getElementById("demo").style.color = "black" }}>HINT</button>
                     </div>
                     <div>
-                        <h1 id='performance-counter'>CORRECTOS:{this.props.performance}</h1>
+                        <h1 id='performance-counter'>CORRECTOS:  {!this.props.percentage ? 0 : Math.floor(this.props.performance/this.props.percentage*100)}%</h1>
                     </div>
                 </main>
             </div>
@@ -132,8 +137,9 @@ const mapStateToProps = state => {
         dogWinnerImage: state.dogs,
         dogWinner: state.dogs.dogWinner,
         dogBreeds: state.dogs.dogBreeds,
-        performance: state.performance
+        performance: state.performance,
+        percentage: state.percentage
     }
 }
 
-export default connect(mapStateToProps, { getWinner, getList, setPerformance })(DogGame)
+export default connect(mapStateToProps, { getWinner, getList, setPerformance, setPercentage })(DogGame)
